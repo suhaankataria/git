4dc53c6d1469d3f4083889824a33283b466e73e2
suhaankataria <suhaankataria@gmail.com>
2025-03-01 10:14:13 +0530

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,3 @@
 print("hello world")
 print("this is a jenkins integration file to test my integrations are working or not")
+print("testing build targets poll scm with every minute automated building")
